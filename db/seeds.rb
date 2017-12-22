327fb0f7b6ec5410cb3bf13483a8f539e2c6eaf2
Max <crono0720@gmail.com>
2017-12-22 23:06:11 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Default Category
+
+Category.destroy_all
+
+category_list = [
+  { name: "中式料理" },
+  { name: "日本料理" },
+  { name: "義大利料理" },
+  { name: "墨西哥料理" },
+  { name: "素食料理" },
+  { name: "美式料理" },
+  { name: "複合式料理" }
+]
+
+category_list.each do |c|
+  Category.create(name: c[:name])
+end
+
+puts "Category created!"
+
+# Default admin
+
+User.create(email: "max@max.com", password: "000000", role: "admin")
+
+puts "Default admin created!"
