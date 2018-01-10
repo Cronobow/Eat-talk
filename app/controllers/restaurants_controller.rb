17aa55082cc1c174e6897e93d00f68c3a14b0aa8
Max <crono0720@gmail.com>
2018-01-10 20:40:38 +0800

    add feeds show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,6 +6,11 @@ class RestaurantsController < ApplicationController
     @categories = Category.all
   end
 
+  def feeds
+    @recent_restaurants = Restaurant.order(created_at: :desc).limit(10)
+    @recent_comments = Comment.order(created_at: :desc).limit(10)
+  end
+
   def show
     @comment = Comment.new
   end
