edc3a3213fa06c0ed0785ca8e266ab42f9cb4003
Max <crono0720@gmail.com>
2018-01-15 16:31:19 +0800

    setup self-referential relationships within users table through followships

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_restaurants, through: :likes, source: :restaurant
 
+  has_many :followships, dependent: :destroy
+  has_many :followings, through: :followships
+
   def is_admin?
     self.role == "admin"
   end
